f0cce8ee3d1fba4d161eb89ec3233390b51afa31
Justin Goh <justin.goh@thoughtworks.com>
2020-06-22 17:27:19 +0800

    get steps and heart rate

--- app/src/main/java/com/radium/laboratories/samsunghealthspike/MainActivity.kt
+++ app/src/main/java/com/radium/laboratories/samsunghealthspike/MainActivity.kt
@@ -28,6 +28,11 @@ class MainActivity : AppCompatActivity() {
                 if (isPermissionAcquired()) {
                     println("got permissions")
                     readTodayStepCountData()
+                    readHeartRate()
+                    print("user")
+                    getUserId()
+                    getDeviceId()
+                    allDevices()
                 } else {
                     println("requesting permissions")
                 }
@@ -41,6 +46,80 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun getUserId() {
+        val usrProfile = HealthUserProfile.getProfile(mStore)
+        println(usrProfile.userName)
+    }
+
+    fun getDeviceId() {
+        val device = HealthDeviceManager(mStore).localDevice
+        val deviceUuid = device.uuid
+        val mfg = device.manufacturer
+        val model = device.model
+        val custom = device.customName
+        println("*********local device")
+        println("$device, $deviceUuid, $mfg, $model, $custom")
+    }
+
+    fun allDevices() {
+        // pass in bfn
+        // if all devices doesnt include bfn, throw error
+        val devices = HealthDeviceManager(mStore).allDevices
+        for (device in devices) {
+            val deviceUuid = device.uuid
+            val mfg = device.manufacturer
+            val model = device.model
+            val custom = device.customName
+            println("*********all devices")
+            println("$device, $deviceUuid, $mfg, $model, $custom")
+        }
+    }
+
+    fun readHeartRate() {
+        val startTime = getTodayStartUtcTime()
+        val ONE_DAY_IN_MS = 24 * 60 * 60 * 1000L
+        val endTime = startTime + ONE_DAY_IN_MS
+
+        val request = HealthDataResolver.ReadRequest.Builder()
+            .setDataType(HealthConstants.HeartRate.HEALTH_DATA_TYPE)
+            .setProperties(arrayOf(
+                HealthConstants.HeartRate.HEART_BEAT_COUNT,
+                HealthConstants.HeartRate.BINNING_DATA,
+                HealthConstants.HeartRate.DEVICE_UUID,
+                HealthConstants.HeartRate.CREATE_TIME,
+                HealthConstants.HeartRate.HEART_RATE
+            ))
+            .setLocalTimeRange(HealthConstants.HeartRate.START_TIME,
+                HealthConstants.HeartRate.TIME_OFFSET, startTime, endTime)
+            .build()
+
+        mResolver = HealthDataResolver(mStore, null)
+
+        try {
+            mResolver?.read(request)?.setResultListener { result ->
+                try {
+                    val iterator = result.iterator();
+                    while (iterator.hasNext()) {
+                        val data = iterator.next()
+                        val heartRate = data.getLong(HealthConstants.HeartRate.HEART_BEAT_COUNT)
+                        val bin = data.getString(HealthConstants.HeartRate.BINNING_DATA)
+                        val uuid = data.getString(HealthConstants.HeartRate.DEVICE_UUID)
+                        val time = data.getString(HealthConstants.HeartRate.CREATE_TIME)
+                        val hr = data.getInt(HealthConstants.HeartRate.HEART_RATE)
+                        println("***************heart rate result")
+                        println("$time, $hr, $uuid")
+                        println("***************heart rate bin")
+                        println("$bin")
+                    }
+                } finally {
+                    result.close()
+                }
+            }
+        } catch (e: Exception) {
+            println("error reading heart rate ${e.message}")
+        }
+    }
+
     fun readTodayStepCountData() {
         val startTime = getTodayStartUtcTime()
         val ONE_DAY_IN_MS = 24 * 60 * 60 * 1000L
@@ -48,7 +127,12 @@ class MainActivity : AppCompatActivity() {
 
         val request = HealthDataResolver.ReadRequest.Builder()
             .setDataType(HealthConstants.StepCount.HEALTH_DATA_TYPE)
-            .setProperties(arrayOf(HealthConstants.StepCount.COUNT))
+            .setProperties(arrayOf(
+                HealthConstants.StepCount.COUNT,
+                HealthConstants.StepCount.CREATE_TIME,
+                HealthConstants.StepCount.DEVICE_UUID
+            ))
+                // TIME_OFFSET needs to set for showing the data's measured time properly???
             .setLocalTimeRange(HealthConstants.StepCount.START_TIME,
                 HealthConstants.StepCount.TIME_OFFSET, startTime, endTime)
             .build()
@@ -57,17 +141,16 @@ class MainActivity : AppCompatActivity() {
 
         try {
             mResolver?.read(request)?.setResultListener { result ->
-                var dayTime = 0L
-                var totalCount = 0
 
                 try {
                     val iterator = result.iterator();
                     while (iterator.hasNext()) {
                         val data = iterator.next()
-                        dayTime = data.getLong("day_time")
-                        totalCount = data.getInt("count")
+                        val totalCount = data.getInt(HealthConstants.StepCount.COUNT) // HealthData
+                        val uuid = data.getString(HealthConstants.StepCount.DEVICE_UUID)
+                        val time = data.getFloat(HealthConstants.StepCount.CREATE_TIME)
                         println("***************result")
-                        println("$dayTime, $totalCount")
+                        println("$time, $totalCount, $uuid")
                     }
                 } finally {
                     result.close()
@@ -88,9 +171,13 @@ class MainActivity : AppCompatActivity() {
             HealthConstants.HeartRate.HEALTH_DATA_TYPE,
             HealthPermissionManager.PermissionType.READ
         )
+        val permKey3 = HealthPermissionManager.PermissionKey(
+            HealthConstants.USER_PROFILE_DATA_TYPE,
+            HealthPermissionManager.PermissionType.READ
+        )
         val pmsManager = HealthPermissionManager(mStore);
         try {
-            pmsManager.requestPermissions(mutableSetOf(permKey, permKey2), this)
+            pmsManager.requestPermissions(mutableSetOf(permKey, permKey2,permKey3), this)
                 .setResultListener { result ->
                     println("Permission callback received")
                     val resultMap = result.resultMap
@@ -137,13 +224,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun getTodayStartUtcTime(): Long {
-        val today = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        val today = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"))
 
         today.set(Calendar.HOUR_OF_DAY, 0)
         today.set(Calendar.MINUTE, 0)
         today.set(Calendar.SECOND, 0)
         today.set(Calendar.MILLISECOND, 0)
 
-        return today.timeInMillis
+        val result = today.timeInMillis
+        println("today time $result")
+        return result
     }
 }
